0841a1a619695ddd891c4cfd5b298aa8ab7a3da5
lliuti <lucas_liuti@hotmail.com>
2019-11-30 22:56:34 -0300

    Tool Model

---
+++ src/app/models/Tool.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Tool extends Model {
+  static init(sequelize) {
+    super.init({
+      title: Sequelize.STRING,
+      link: Sequelize.STRING,
+      description: Sequelize.STRING,
+      tags: Sequelize.ARRAY(Sequelize.STRING),
+    },
+    {
+      sequelize,
+    },
+    );
+
+    return this;
+  
+  };
+
+};
+
+export default Tool;
