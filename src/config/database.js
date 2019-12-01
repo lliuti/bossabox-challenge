31858ea15482243308fec3e3244594e4bc6267d2
lliuti <lucas_liuti@hotmail.com>
2019-11-30 22:56:00 -0300

    Setting up database
    In here I'm setting up the database connection and I'm also creating a model loader

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  dialect: 'postgres',
+  username: 'postgres',
+  password: 'postgresdocker',
+  host: 'localhost',
+  port: '5432',
+  database: 'VUTTR',
+  define: {
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true,
+  },
+
+};
---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+import Tools from '../app/models/Tool';
+import dbConfig from '../config/database';
+
+const models = [Tools];
+
+class Database {
+  
+  constructor() { 
+    this.init();
+  };
+
+  init() {
+    this.connection = new Sequelize(dbConfig);
+
+    models
+      .map(model => model.init(this.connection))
+  };
+
+}
+
+export default new Database();
