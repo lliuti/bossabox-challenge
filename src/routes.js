26c1df06ac722926d6b80da71697ff1537032cd1
lliuti <lucas_liuti@hotmail.com>
2019-12-05 17:13:34 -0300

    Updating routes
    Added:
    - post /users
    - post /sessions
    
    and also using the SessionMiddleware

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,23 @@
 import { Router } from 'express';
 import ToolController from './app/controllers/ToolController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import SessionMiddleware from './app/middlewares/SessionMiddleware';
 
 const routes = new Router();
 
+// Creates a new user
+routes.post('/users', UserController.store);
+
+// Authenticates user
+routes.post('/sessions', SessionController.store);
+
 // Finds either all Tools or search them by it's Tags
 routes.get('/tools', ToolController.find);
+
+// If the user is not authenticated, it will not allow
+// him to go through the next routes
+routes.use(SessionMiddleware);
 
 // Adds new Tools
 routes.post('/tools', ToolController.store);
