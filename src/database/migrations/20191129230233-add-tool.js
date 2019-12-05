c5218ea4a6aad41f5e1467dbbccc256c883ab3e4
lliuti <lucas_liuti@hotmail.com>
2019-12-05 16:54:16 -0300

    adding timestamps to Tool table

--- src/database/migrations/20191129230233-add-tool.js
+++ src/database/migrations/20191129230233-add-tool.js
@@ -24,6 +24,14 @@ module.exports = {
         type: Sequelize.ARRAY(Sequelize.STRING),
         allowNull: true,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
