945e45a8ca38e1297927898c6a2f05899a3b614a
lliuti <lucas_liuti@hotmail.com>
2019-12-05 17:01:09 -0300

    creating SessionController

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    // Tries to find an User with passed email
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    if (!user) return res.json({ error: 'User not found' });
+
+    const { id, name } = user;
+
+    // Verifies password
+    if (!(await user.checkPassword(password)))
+      return res.json({ error: 'Invalid password' });
+
+    return res.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
